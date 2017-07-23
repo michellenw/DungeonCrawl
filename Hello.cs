50f5aefe7eb5a3afd7039c6769ca292c772654b6
Michelle Wysocki <michellewyso@gmail.com>
2017-07-23 13:42:45 -0400

    First commit

---
+++ Hello.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Helloworld
+{
+	public static void Main()
+	{
+		System.Console.WriteLine("Hello, World!");
+	}
+}
+
---
+++ dungeoncrawl.cs
@@ -0,0 +1,148 @@
+using System;
+
+namespace DungeonCrawl {
+
+	private class Program {
+
+		public static void Main(string[] args) {
+			Player player = new Player();
+			
+			Console.WriteLine("Welcome. Type your name and hit enter:");
+			player.name = Console.ReadLine();
+			
+			Console.WriteLine("What species are you?\n");
+			Console.WriteLine("Type: 1 for Archer, 2 for Mage, 3 for Warrior");
+			int chosenRole = Console.ReadLine();
+			while  ((chosenRole != 1) || (chosenRole != 2) || (chosenRole != 3)) {
+				Console.WriteLine("That is not a valid input. Pick 1, 2 or 3.\n");
+			        Console.WriteLine("What species are you?\n");
+                                Console.WriteLine("Type: 1 for Archer, 2 for Mage, 3 for Warrior");
+				int chosenRole = Console.ReadLine();
+			}
+			player.role = chosenRole;
+
+
+		}
+
+                // *****************************************************************************************
+
+		public class Living {
+                        public int name { get; set; };
+		        public int health { get; set; };
+		}
+
+                // *****************************************************************************************		
+
+		// Player is now a child class, or derived class, from the Living class
+		public class Player : Living {
+			// get set signifies that we have a property where we can get a value and set a value 
+	 	        public int role { get; set; };
+			
+			// constructor			
+			public Player() {
+				this.health = 10;
+			}
+
+			public string getRole() {
+				if (role == 1) {
+					return "Archer";
+				}
+				else if (role == 2) {
+					return "Mage";
+				}
+				else {
+					return "Warrior";
+				}
+			}
+
+			public playerInfo() {
+				return String.Format("{0} {1} - {2} health left!", getRole(), this.name, this.health);
+			}
+			
+		}
+
+                // *****************************************************************************************
+
+		public class Monster : Living {
+			
+			public Monster(string name, int health) {
+				this.name = name;
+				this.health = health;
+			}
+			
+			public monsterInfo() {
+				return String.Format("{0} with {1} health left!", this.name, this.health);
+			}
+		}
+
+	        // *****************************************************************************************
+
+		public class Location {
+			public Player player { get; set; }
+			public string location { get; set; }
+			public int depth { get; set; }
+
+			// constructor
+			public Location(Player player, int depth) {
+				this.player = player;
+				this.depth = depth;
+
+				// levels of depth
+				if (this.depth == 0) {
+					this.location == "Underground Realm";
+					Console.WriteLine(welcomeMessage(this.location));
+
+					Monster caveSpider = new Monster("Cave Spider", 5);
+					Battle battle0 = new Battle(player, caveSpider);
+				}	
+				else if (this.depth == 1) {	
+					this.location == "Cavern";
+					Console.WriteLine(welomeMessage(this.location));
+
+					Monster creeper = new Monster("Creeper", 10);
+					Battle battle1 = new Battle(player, creeper);
+				}
+				else if (this.depth == 2) {
+					this.location == "Underworld";
+					Console.WriteLine(welcomeMessage(this.location));
+
+					Monster enderdragon = new Monster("Enderdragon", 15);
+					Battle battle2 = new Battle(player, enderdragon);
+				}
+			}
+
+			public string welcomeMessage(string location) {
+				return this.player.name + " has entered the " + location + "\n";
+			}
+		}
+
+		// *****************************************************************************************
+
+		public class Battle {
+			
+			public Player player { get; set; }
+			public Monster monster { get; set; }
+			public bool yourTurn { get; set; }
+			public int playerAttack { get; set; } 
+
+			public Battle(Player player, Monster monster) {
+				this.player = player;
+				this.enemy = enemy;
+				// allow player to start battle
+				this.turn = true;
+
+				Console.WriteLine("A {0} has appeared! Get ready to battle...", this.monster.name);
+				
+				// Battle loop
+			}
+			public void endMessage (bool end) {
+				if(end) {
+					Console.WriteLine("You have defeated {0}!\n", this.monster.name);
+				}
+				else {
+					Console.WriteLine("You have defeated {0}! You beat the dungeon!", this.monster.name);
+				}
+			}
+		} 		
+	}		
+}
